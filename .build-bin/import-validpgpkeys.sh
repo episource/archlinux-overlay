f57f47707974e2f2ddfe9280417ed9e6d6a66856
Philipp Serr <episource@gmx.de>
2017-07-21 21:35:47 +0200

    Fetch gpg keys using IPv4

--- .build-bin/import-validpgpkeys.sh
+++ .build-bin/import-validpgpkeys.sh
@@ -4,6 +4,10 @@ source "$SCRIPT_DIR/../.build-lib/ci-library.sh"
 
 _do list_packages
 
+# disable ipv6 for key fetching (inside docker!)
+# dirmngr must not yet be running
+mkdir -p "$HOME/.gnupg"
+echo "disable-ipv6" > "$HOME/.gnupg/dirmngr.conf"
 
 # `gpg --recv-key` requires write access to the current user's home directory!
 _do gpg --recv-key $(get_validpgpkeys)
